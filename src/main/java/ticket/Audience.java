85b922ca4c15f630848868d1f919825641f6ff4d
gray <operasystem6@naver.com>
2024-02-08 09:52:46 +0900

    티켓 판매 애플리케이션 1

---
+++ src/main/java/ticket/Audience.java
@@ -0,0 +1,31 @@
+package ticket;
+
+public class Audience {
+
+    private Bag bag;
+
+    public Audience(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Bag getBag() {
+        return bag;
+    }
+
+    public void setBag(Bag bag) {
+        this.bag = bag;
+    }
+
+    public Long buy(Ticket ticket) {
+        if (bag.hasInvitation()) {
+            bag.setTicket(ticket);
+            return 0L;
+        } else {
+            bag.minusAmount(ticket.getFee());
+            bag.setTicket(ticket);
+            return ticket.getFee();
+        }
+    }
+}
+
+
---
+++ src/main/java/ticket/Invitation.java
@@ -0,0 +1,12 @@
+package ticket;
+
+import java.time.LocalDateTime;
+
+public class Invitation {
+    private LocalDateTime when;
+
+    public LocalDateTime getWhen() {
+        return when;
+    }
+}
+
